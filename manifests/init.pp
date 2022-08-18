6a6d8aaebb8a5aae8503144db54cf409e6d46f00
Andrew Jones <andrew.jones@puppet.com>
2022-08-18 15:49:23 +0100

    initial

---
+++ manifests/init.pp
@@ -0,0 +1,44 @@
+# Class: trellix_agent
+#
+# Install and configure the Trellix agent on Linux
+#
+# === Parameters
+#
+# @param agent_install_script
+#   String
+#   Installation script being run.
+#
+# @param agent_install_options
+#   String
+#   Installation options for script being run.
+#   Default -i to install
+#
+# @param agent_service_name
+#   String
+#   Name of the Agent Service
+#
+class mcafee_epo_agent (
+  String  $agent_install_script   = 'puppet:///modules/files/install.sh',
+  String  $agent_install_options  = '-i',
+  String  $agent_service_name     = 'ma',
+) {
+
+  file{ '/tmp/install.sh':
+    ensure => 'present',
+    source => $agent_install_script,
+    mode   => '0700',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  exec{ 'Install':
+    command => "/tmp/install.sh ${agent_install_options}",
+    creates => '/opt/McAfee/agent/license.txt',
+    user    => root,
+  }
+
+  service{ $agent_service_name:
+    ensure => running,
+    enable => true,
+  }
+}
