9f309ad66d20e116bfc1987a525343fb0f6487e2
Andrew Jones <andrew.jones@puppet.com>
2024-04-22 17:47:03 +0100

    pdk 3 test

---
+++ manifests/epa.pp
@@ -0,0 +1,26 @@
+#
+class trellix_agent::epa {
+
+  package { 'epa':
+    ensure => installed,
+    source => 'puppet///modules/files/epa.rpm',
+  }
+
+  file { '/var/tmp/EPAinstall.sh':
+    ensure  => file,
+    source  => 'puppet///modules/files/epa.sh',
+    require => Package[''],
+  }
+
+  exec { 'install epa':
+    command => 'yes "" | /var/tmp/EPAinstall.sh -i',
+    creates => '/var/eracent.epa/epa.sh',
+    require => File['/var/tmp/EPAinstall.sh'],
+  }
+
+  service { 'EracentEUAService':
+    ensure  => running,
+    enable  => true,
+    require => Exec['install epa'],
+  }
+}
